217c134f4c50df40c8ea827138036330c07e3c7a
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 21:45:23 -0200

    Exercicio do capitulo 1

---
+++ exercicios/capitulo-1.py
@@ -0,0 +1,97 @@
+# Quais das opções a seguir são operadores e quais são valores?
+'''
+*
+'Hello'
+-88.8
+-
+/
++
+5
+'''
+
+''' 
+Resposta:
+Operador
+Valor
+Valor
+Operador
+Operador
+Operador
+Valor
+'''
+
+# 2 Qual das opcoes a seguir é uma variavel e qual é uma string?
+'''
+spam
+'spam'
+'''
+
+'''
+Resposta
+Variavel
+String
+'''
+
+# 3 Nomeie três tipos de dados
+
+
+# 4 De que é composta uma expressao? O que fazem as expressoes?
+'''
+Uma expressão é composta por operadores aritméticos.
+Os operadores aritméticos possuem precedência para realizar o calculo
+Podemos criar expressões utilizando parenteses e os operadores aritméticos
+'''
+
+# 5 Foi apresentado as instruções de atribuicao, por exemplo spam = 10. Qual é a diferenca entre uma expressao e uma instrucao?
+'''
+Uma expressao é utilizada para realizar os calculos aritméticos, já uma instrução de atribuição ela seta um valor para uma determinada variável
+'''
+
+# 6 O que a variável bacon conterá após o código a seguir ser executado?
+'''
+bacon = 20
+bacon + 1
+'''
+
+'''
+Resposta: 21
+'''
+
+# 7 Para que valores das duas expressões a seguir serão avaliadas?
+'''
+'spam' + 'spamspam'
+'spam' * 3
+'''
+
+'''
+Resposta:
+'spamspamspam'
+'spamspamspam'
+'''
+
+# 8 Por que eggs é um nome válido enquanto 100 é inválido?
+'''
+eggs encontra-se valido pois utiliza somente letras, já o 100 não é valido pq é iniciado por números
+'''
+
+# 9 Quais três funcoes podem ser usadas para obter uma versão inteira, de ponto flutuante ou em string de um valor?
+'''
+int(), float(), str()
+'''
+
+# 10 Por que a expressao a seguir causa um erro? Como você pode corrigi-la?
+'''
+'I have eaten ' + 99 + ' burritos.'
+'''
+
+'''
+Resposta
+Por que não se pode somar inteiros com strings
+Solução: 'I have eaten ' + str(99) + ' burritos.'
+'''
+
+# Extra
+'''
+Pesquisa a documentacao do Python online em busca da função len(). Ela esta em uma pagina web chamada "Built-in Functions" (Funcções internas)
+Passe o olho pela lista das demais funções Python, verifique o que a função round() faz e realize experimentos com ela no shell interativo.
+'''
