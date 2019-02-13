5a607f2140089eebb453aed57f1422cb576732f5
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 21:12:46 -0200

    Variaveis

---
+++ 1 - basico/variaveis.py
@@ -0,0 +1,30 @@
+# Nomes validos para variaveis
+'''
+balance
+currentBalance
+current_balance
+_spam
+SPAM
+account4
+'''
+
+# Nomes invalidos para variaveis
+'''
+current-balance (hifens nao sao permitidos)
+current balance (espacos nao sao permitidos)
+4account (nao pode comecar com um numero)
+total_$um (caracteres especiais como $ nao sao permitidos)
+'hello' (caracteres especiais como ' nao sao permitidos)
+'''
+
+spam = 40
+eggs = 2
+txt = 'Hello'
+
+print(spam)
+print(spam + eggs)
+print(spam + eggs + spam)
+print(txt)
+
+txt = 'Goodbye'
+print(txt)
