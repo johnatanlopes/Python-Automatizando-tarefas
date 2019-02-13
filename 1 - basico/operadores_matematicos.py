81de894bed782c0ea90f87ba2b847c5a8c4c7b31
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 21:03:00 -0200

    Operadores matematicos

---
+++ 1 - basico/operadores_matematicos.py
@@ -0,0 +1,19 @@
+'''
+Operadores matematicos, do maior para o menor nivel de precedencia
+** exponencial Ex: 2 ** 3 = 8
+% modulo/resto Ex: 22 % 8 = 6
+// divisao inteira Ex: 22 // 8 = 2
+/ divisao Ex: 22 / 8 = 2.75
+* multiplicacao Ex: 3 * 5 = 15
+- subtracao Ex: 5 - 2 = 3
++ adicao 2 + 2 = 4
+'''
+
+# Realizando calculos
+print(2 + 2)
+print((2 + 2) * 6)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(23 % 7)
+print((5 - 1) * ((7 + 1) / 3 - 1))
