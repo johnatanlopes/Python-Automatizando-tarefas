49fdb4565f5c5b7f3b498875e2e30e4743511f32
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 21:52:48 -0200

    Valores booleanos e operadores de comparacao

---
+++ 2 - controle de fluxo/valores-booleanos_e_operadores-de-comparacao.py
@@ -0,0 +1,36 @@
+# Booleanos
+'''
+O tipo de dado booleano (Boolean) tem apenas dois valores: True ou False
+'''
+
+# Operadores de comparação
+'''
+== igual a
+!= diferente de 
+< menor que
+> maior que
+<= menor ou igual a
+>= maior ou igual a
+'''
+
+print(42 == 42)
+print(42 == 99)
+print(2 != 3)
+print(2 != 2)
+print('hello' == 'hello')
+print('hello' == 'Hello')
+print('dog' != 'cat')
+print(True == True)
+print(True != False)
+print(42 == 42.0)
+print(42 == '42')
+
+print(42 < 100)
+print(42 > 100)
+print(42 < 42)
+
+eggCount = 42
+print(eggCount <= 42)
+
+myAge = 29
+print(myAge >= 10)
