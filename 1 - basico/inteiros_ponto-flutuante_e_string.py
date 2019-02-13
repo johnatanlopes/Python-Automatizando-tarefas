f25788d78837ba446dc80756c6cbc12a5922b8c3
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 21:08:35 -0200

    Inteiros, ponto flutuante e string

---
+++ 1 - basico/inteiros_ponto-flutuante_e_string.py
@@ -0,0 +1,20 @@
+'''
+Tipos comuns de dados
+Inteiros - -2, -1, 0, 1, 2, 3, 4, 5,
+Ponto flutuante -1.25, -1.0, --0.5, 0.0, 0.5, 1.0, 1.25
+Strings 'a', 'aa', 'aaa', 'Hello!', '11 cats'
+'''
+
+# Concatenacao e repeticao de strings
+# O operador de adicao + é utilizado para juntar as strings
+print('Alice' + 'Bob')
+
+# Caso tente concatenar string mais inteiro ocorrerá erro
+# print('Alice' + 23)
+
+# O operador de multiplicacao é utilizado para multiplicar a quantidade de strings
+print('Alice' * 5)
+
+# Ocorrerá erro ao tentar utilizar o * com duas strings
+# print('Alice' * 'Bob')
+
