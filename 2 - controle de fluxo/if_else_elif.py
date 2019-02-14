244ac7de3f2e4634e8fe86c9d53ccdeebc4d1cea
johnatanlopes <johnatanlopes@gmail.com>
2019-02-13 22:14:31 -0200

    if, else e elif

---
+++ 2 - controle de fluxo/if_else_elif.py
@@ -0,0 +1,20 @@
+name = 'Alice'
+
+# Instrução if
+if name == 'Alice':
+    print('Hi, Alice!')
+
+
+# Instrução if else
+if name == 'Jose':
+    print('Hi, Jose')
+else:
+    print('Hello, stranger.')
+
+
+# Instrução if elif
+age = 11
+if name == 'Jose':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Jose, kiddo')
---
+++ 2 - controle de fluxo/vampiro.py
@@ -0,0 +1,13 @@
+name = 'Jose'
+age = 90
+
+if name == 'Alice':
+    print('Hi, Alice!')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 100:
+    print('You are not Alice, grannie')
+elif age > 2000:
+    print('Unlinke you, Alice is not an undead, immortal vampire')
+else:
+    print('You are neither Alice nor a little kid')
