9eef6738c7734ed83c8aa8a0cb4cccdf9f22c0fa
johnatanlopes <johnatanlopes@gmail.com>
2019-02-14 21:39:07 -0200

    Criando nosso programa de frases com lista e aprendendo sobre funcoes list e tuple

---
+++ 4 - listas/magicBall.py
@@ -0,0 +1,6 @@
+import random
+
+messages = ['It is certain', 'It is decidely so', 'Yes definitely', 'Reply hazy try again', 'Ask again later', 'Concentrate and ask again',
+    'My Reply is no', 'Outlook no so good', 'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
