f966de109f0c0eb9a5f30393148acf5512e56105
johnatanlopes <johnatanlopes@gmail.com>
2019-02-15 17:02:56 -0200

    Metodos startswith e endswith

---
+++ 6 - manipulando strings/metodos-startswitch-e-endswitch.py
@@ -0,0 +1,10 @@
+# startswith() - Se começar com o valor procurado retorna True
+print('Hello world!'.startswith('Hello'))
+print('Hello world!'.startswith('Hello world!'))
+print('abc123'.startswith('abcdef'))
+print('abc123'.startswith('12'))
+
+# endswith() - Se terminar com o valor procurado retorna True
+print('Hello world!'.endswith('world!'))
+print('Hello world!'.endswith('Hello world!'))
+print('abc123'.endswith('12'))
