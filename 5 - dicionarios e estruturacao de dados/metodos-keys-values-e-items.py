336aa7840651ac226b1f49077282783495189121
johnatanlopes <johnatanlopes@gmail.com>
2019-02-15 01:57:52 -0200

    Metodos keys, values e items

---
+++ 5 - dicionarios e estruturacao de dados/metodos-keys-values-e-items.py
@@ -0,0 +1,22 @@
+'''
+O método values() retorna os valores que estão armazenados em cada indice
+'''
+spam = {'color': 'red', 'age': 42}
+for v in spam.values():
+    print(v)
+
+'''
+O método keys retorna os indices
+'''
+for k in spam.keys():
+    print(k)
+
+'''
+O método items retorna tuplas contendo o indice e o valor
+'''
+for i in spam.items():
+    print(i)
+
+
+
+
