fd6e7352084bd70229428ad33e31c4e1c9a225e3
johnatanlopes <johnatanlopes@gmail.com>
2019-02-16 19:39:17 -0200

    Diretorios absolutos e relativos

---
+++ 8 - lendo e escrevendo em arquivos/comparacao-path-absoluto-e-relativo.py
@@ -0,0 +1,8 @@
+'''
+Um path absoluto, que sempre começa com a pasta raiz.
+Um path relativo, que é relativo ao diretório de trabalho atual do programa.
+
+Temos também as pastas ponto (.) e ponto-ponto (..). Essas não são pastas ded verdade, mas nomes especiais que podem ser usados
+em um path. Um único ponto é abreviação para este diretório, dois pontos quer dizer a pasta pai
+''' 
+
