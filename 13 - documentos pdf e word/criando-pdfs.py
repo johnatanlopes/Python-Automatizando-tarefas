49921c9c5b25c0db98be85320dbd60e7bfe5353d
johnatanlopes <johnatanlopes@gmail.com>
2019-02-20 21:06:35 -0300

    Copiando pdfs e rotacionando paginas

---
+++ 13 - documentos pdf e word/criando-pdfs.py
@@ -0,0 +1,49 @@
+'''
+A contrapartida do PyPDF2 ao objeto PdfFileReader é o objeto PdfFileWriter, que pode criar novos arquivos PDF. Porém não pode escrever qualquer texto em um
+PDF, como o Python pode fazer com arquivos em formato texto simples. As capacidades de escrita de PDF do PyPDF2 estão limitadas a copiar páginas de outros PDFs,
+fazer rotação de páginas, sobrepor páginas e criptografar arquivos.
+
+O PyPDF2 não permite editar diretamente um PDF, em vez disso voce deverá criar um novo PDF e então copiar o conteúdo de um documento existente. 
+'''
+
+'''
+Copiando páginas
+'''
+
+import PyPDF2
+pdf1File = open('meetingminutes.pdf', 'rb')
+pdf2File = open('meetingminutes2.pdf', 'rb')
+pdf1Reader = PyPDF2.PdfFileReader(pdf1File)
+pdf2Reader = PyPDF2.PdfFileReader(pdf2File)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pageNum in range(pdf1Reader.numPages):
+    pageObj = pdf1Reader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+
+for pageNum in range(pdf2Reader.numPages):
+    pageObj = pdf2Reader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+
+pdfOutputFile = open('combinedminutes.pdf', 'wb')
+pdfWriter.write(pdfOutputFile)
+pdfOutputFile.close()
+pdf1File.close()
+pdf2File.close()
+
+
+'''
+Rotação de páginas
+'''
+
+minutesFile = open('meetingminutes.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(minutesFile)
+page = pdfReader.getPage(0)
+page.rotateClockwise(90)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+pdfWriter.addPage(page)
+resultPdfFile = open('rotatedPage.pdf', 'wb')
+pdfWriter.write(resultPdfFile)
+resultPdfFile.close()
+minutesFile.close()
